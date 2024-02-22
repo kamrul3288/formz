8bc8d43af01dcd5e2e08e94433d19447b05e50c1
Kamrul Hasan <kamrulhasan3288@gmail.com>
2024-02-22 12:23:36 +0600

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Formz"
+include(":compose-example")
+include(":mdc-example")
